9b082b04a1fd165c0cb1579ee995a3952daabc20
jtking00 <122916900+jtking00@users.noreply.github.com>
2023-02-28 10:09:20 -0800

    dataaccess and models for Week 7 Lab

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
+import java.io.Serializable;
+
 /**
- *
  * @author 882199
  */
-public class User {
-    
+public class User implements Serializable{
+    String email;
+    String fName;
+    String lName;
+    String password;
+    Role role;
+    User(){
+    }
+    User(String email, String fName, String lName, String password, Role role){
+        this.email = email;
+        this.fName = fName;
+        this.lName = lName;
+        this.password = password;
+        this.role = role;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getFName(){
+        return this.fName;
+    }
+    public void setFName(String fName){
+        this.fName = fName;
+    }
+    public String getLName(){
+        return this.lName;
+    }
+    public void setLName(String lName){
+        this.lName = lName;
+    }
+    public String getPass(){
+        return this.password;
+    }
+    public void setPass(String password){
+        this.password = password;
+    }
+    public Role getRole(){
+        return this.role;
+    }
+    public void setRole(Role role){
+        this.role = role;
+    }
 }
